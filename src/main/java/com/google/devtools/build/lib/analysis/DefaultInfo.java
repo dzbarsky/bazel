3226208e8d940cf45e01bfa329a96124aa82f80b
David Zbarsky <dzbarsky@gmail.com>
2026-08-15 15:28:25 -0400

    Expose parent rule executables through DefaultInfo.files_to_run

--- src/main/java/com/google/devtools/build/lib/analysis/DefaultInfo.java
+++ src/main/java/com/google/devtools/build/lib/analysis/DefaultInfo.java
@@ -16,6 +16,8 @@ package com.google.devtools.build.lib.analysis;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.analysis.configuredtargets.AbstractConfiguredTarget;
 import com.google.devtools.build.lib.collect.nestedset.Depset;
+import com.google.devtools.build.lib.collect.nestedset.NestedSetBuilder;
+import com.google.devtools.build.lib.collect.nestedset.Order;
 import com.google.devtools.build.lib.concurrent.ThreadSafety.Immutable;
 import com.google.devtools.build.lib.packages.BuiltinProvider;
 import com.google.devtools.build.lib.packages.NativeInfo;
@@ -94,15 +96,20 @@ public abstract class DefaultInfo extends NativeInfo implements DefaultInfoApi {
         Runfiles runfiles,
         Runfiles dataRunfiles,
         Runfiles defaultRunfiles,
-        Artifact executable,
-        @Nullable FilesToRunProvider filesToRunProvider) {
+        Artifact executable) {
       super(loc);
       this.files = files;
       this.runfiles = runfiles;
       this.dataRunfiles = dataRunfiles;
       this.defaultRunfiles = defaultRunfiles;
       this.executable = executable;
-      this.filesToRunProvider = filesToRunProvider;
+      this.filesToRunProvider =
+          executable == null
+              ? null
+              : FilesToRunProvider.create(
+                  NestedSetBuilder.create(Order.STABLE_ORDER, executable),
+                  /* runfilesSupport= */ null,
+                  executable);
     }
 
     @Override
@@ -222,8 +229,7 @@ public abstract class DefaultInfo extends NativeInfo implements DefaultInfoApi {
           statelessRunfiles,
           dataRunfiles,
           defaultRunfiles,
-          castNoneToNull(Artifact.class, executable),
-          null);
+          castNoneToNull(Artifact.class, executable));
     }
   }
 
