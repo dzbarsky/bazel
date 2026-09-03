e3f5b712b95732af513f290b5be868dcdf86ad97
iancha1992 <heec@google.com>
2026-09-03 11:37:04 -0700

    Fix for compatibility
    (cherry picked from commit bb9659dc3d2fc3632d0554649bb12be0632132e2)

--- src/main/java/com/google/devtools/build/lib/remote/RemoteModule.java
+++ src/main/java/com/google/devtools/build/lib/remote/RemoteModule.java
@@ -1064,7 +1064,9 @@ public final class RemoteModule extends BlazeModule {
       AnalysisResult analysisResult) {
     BuildRequestOptions buildRequestOptions =
         env.getOptions().getOptions(BuildRequestOptions.class);
-    if (remoteOutputChecker != null && buildRequestOptions.getPerformExecutionPhase()) {
+    if (remoteOutputChecker != null
+        && buildRequestOptions != null
+        && buildRequestOptions.performExecutionPhase) {
       remoteOutputChecker.afterAnalysis(analysisResult);
     }
   }
